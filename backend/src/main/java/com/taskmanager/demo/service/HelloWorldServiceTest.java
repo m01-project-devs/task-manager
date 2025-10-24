043fc89c6f1f7e7fea568d8bc5300263de063fdd
TimurSatarov <timursatarov3@gmail.com>
2025-10-24 11:17:43 +0200

    testing file

--- backend/src/main/java/com/taskmanager/demo/service/HelloWorldServiceTest.java
+++ backend/src/main/java/com/taskmanager/demo/service/HelloWorldServiceTest.java
@@ -2,10 +2,22 @@ package com.taskmanager.demo.service;
 
 
 
-import org.junit.Test;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class HelloWorldServiceTest
 {
-
-
+    private final HelloWorldService helloWorldService = new HelloWorldService();
+    @Test
+    public void testHelloWorldServiceDefault()
+    {
+        String result = helloWorldService.sayHello(null);
+        assertEquals(result, "Hello World!");
+    }
+    public void testHelloWorldServiceWithName()
+    {
+        String result = helloWorldService.sayHello("Jack");
+        assertEquals(result, "Hello Jack!");
+    }
 }
