aac33137a24bf15d19fe87f5b6b4d938d3fd319a
shermatov <aibekshermatovv@gmail.com>
2025-11-01 13:44:48 +0100

    use lombok annotations and implement entity model

---
+++ backend/src/main/java/com/m01project/taskmanager/demo/model/User.java
@@ -0,0 +1,31 @@
+package com.m01project.taskmanager.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, updatable = false, name="created_at")
+    private LocalDateTime createdAt =  LocalDateTime.now();
+}
