e45e1d09d4b97d1819c4b85f88b162d8fc21a6ba
Ihlasthetics <yhlascary04@gmail.com>
2025-12-09 11:57:26 +0100

    Use centralized API_BASE_URL from VITE_API_URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
 import HomePage from "./pages/HomePage";
+import { API_BASE_URL } from './config/apiConfig'
 
 function App() {
-  console.log(import.meta.env.VITE_API_URL);
+  console.log('API_BASE_URL:', API_BASE_URL );
 
   return (
     <>
@@ -19,6 +20,9 @@ function App() {
         <Typography variant="h3" gutterBottom>
           Welcome to Frontend, Abiler!
         </Typography>
+         <Typography>
+             API base URL: {API_BASE_URL}
+         </Typography>
         <Button variant="contained">MUI Works!</Button>
         <HomePage/>
       </Container>
