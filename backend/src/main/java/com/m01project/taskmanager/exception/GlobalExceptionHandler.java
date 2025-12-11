71ca13875d29287bba869b3d7f1387c138955dfc
Sakarya,Hamza Ali <asakarya@rbbn.com>
2025-12-11 19:00:53 +0100

    DEV-12 Added MethodArgumentNotValidException and ConstraintViolationException to GlobalExceptionHandler

--- backend/src/main/java/com/m01project/taskmanager/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/m01project/taskmanager/exception/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.m01project.taskmanager.exception;
 
 import com.m01project.taskmanager.dto.response.ErrorResponseDto;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class GlobalExceptionHandler {
 
@@ -27,6 +30,38 @@ public class GlobalExceptionHandler {
                 HttpStatus.CONFLICT.value()
         );
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        ErrorResponseDto error = new ErrorResponseDto(
+                LocalDateTime.now(),
+                errorMessage,
+                HttpStatus.BAD_REQUEST.value()
+        );
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponseDto> handleConstraintViolation(ConstraintViolationException ex) {
+
+        String errorMessage = ex.getConstraintViolations()
+                .stream()
+                .map(v -> v.getPropertyPath() + ": " + v.getMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorResponseDto error = new ErrorResponseDto(
+                LocalDateTime.now(),
+                errorMessage,
+                HttpStatus.BAD_REQUEST.value()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
