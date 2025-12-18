c1e28c2530c76ca762390c05ada7fde2b5729908
Toyli-04 <agajumayevtoyly@gmail.com>
2025-12-18 09:55:58 +0530

    DEV-16 Generated project layers

--- backend/src/main/resources/db/migration/V2__create_boards_table.sql
+++ backend/src/main/resources/db/migration/V2__create_boards_table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE boards (
+CREATE TABLE board (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     user_id BIGINT NOT NULL,
     is_deleted BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT fk_boards_user
+    CONSTRAINT fk_board_user
         FOREIGN KEY (user_id)
         REFERENCES users(id)
 );
