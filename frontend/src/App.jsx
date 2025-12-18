d91a98c2a594699f0f97d4d035c4a1992d4ae445
Adiletbaike <adiletdzhuraev@gmail.com>
2025-12-18 08:51:40 +0100

    DEV-25 Implement registration and login pages with React Router integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,31 @@
-import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Box } from "@mui/material";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
-import { API_BASE_URL } from './config/apiConfig'
 
 function App() {
-  console.log('API_BASE_URL:', API_BASE_URL );
-
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            My React + MUI App
-          </Typography>
-          <Button color="inherit" sx={{ background: "green", }}>Login</Button>
-        </Toolbar>
-      </AppBar>
-
-      <Container sx={{ background: '#DBE9F4', minHeight: "100vh" }} maxWidth={false}>
-        <Typography variant="h3" gutterBottom>
-          Welcome to Frontend, Abiler!
-        </Typography>
-        <Button variant="contained">MUI Works!</Button>
-        <HomePage/>
-      </Container>
-      
-    </>
+    <BrowserRouter>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundImage: "url('./public/background.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/register" replace />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/homepage" element={<HomePage />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
