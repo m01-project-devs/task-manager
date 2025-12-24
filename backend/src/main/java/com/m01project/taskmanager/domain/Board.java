fda209c4444a6954cbdd4a60074e7f723cce40ef
Toyli-04 <agajumayevtoyly@gmail.com>
2025-12-24 13:06:22 +0530

    DEV-16 Added unit tests for BoardService

--- backend/src/main/java/com/m01project/taskmanager/domain/Board.java
+++ backend/src/main/java/com/m01project/taskmanager/domain/Board.java
@@ -1,32 +1,21 @@
 package com.m01project.taskmanager.domain;
 
+import com.m01project.taskmanager.domain.Base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "board")
-public class Board {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Board extends BaseEntity {
 
     @Column(nullable = false)
     private String name;
 
-    @Column(name = "is_deleted", nullable = false)
-    private boolean deleted = false;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt =  LocalDateTime.now();
-
 }
